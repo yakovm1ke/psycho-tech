05a6a80c290bf6718f8fc80b3490e79d468dc827
Yakovlev Mikhail Alekseevich <mikhayakovlev@ozon.ru>
2022-09-06 15:37:02 +0300

    generate project

---
+++ src/views/ErrorView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>asd;lkas;ldk;las,as,;d</div>
+  <div>asd;lkas;ldk;las,as,;d</div>
+  <div>asd;lkas;ldk;las,as,;d</div>
+  <div>asd;lkas;ldk;las,as,;d</div>
+  <div>asd;lkas;ldk;las,as,;d</div>
+  <main>Ничего не найдено</main>
+</template>
