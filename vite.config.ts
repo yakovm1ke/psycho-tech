6cabc5210aeb43d74cfe70486a426f485e643ea0
Yakovlev Mikhail Alekseevich <yakovmikemikle@gmail.com>
2022-09-10 18:08:10 +0300

    add deploy

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,12 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: '/psycho-tech/',
+  build: {
+    outDir: './docs',
+    rollupOptions: {
+      
+    }
+  },
 })
